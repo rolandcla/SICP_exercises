ae2102dce6604212e0a860cc4063bc5cc447afb5
Roland Clairquin <roland.clairquin@aeronomie.be>
2016-08-14 10:37:06 +0200

    Jusque exercice 2.65.

--- exercises_2_3.scm
+++ exercises_2_3.scm
@@ -4,14 +4,11 @@
 ;; Exercise 2.54
 ;; -------------
 
-(define (equal? x y)
-  (display x)
-  (display y)
-  (newline)
-  (if (and (pair? x) (pair? y))
-      (and (equal? (car x) (car y)) (equal? (cdr x) (cdr y)))
-      (eq? x y)
-      ))
+;; (define (equal? x y)
+;;   (if (and (pair? x) (pair? y))
+;;       (and (equal? (car x) (car y)) (equal? (cdr x) (cdr y)))
+;;       (eq? x y)
+;;       ))
 
 ;;-----------------------------------------------------------------------------
 
@@ -211,6 +208,217 @@
 (define (base x) (car x))
 
 (define (exponent x) (caddr x))
+
+;;-----------------------------------------------------------------------------
+
+;; (define (element-of-set? x set)
+;;   (cond ((null? set) false)
+;;         ((equal? x (car set)) true)
+;;         (else (element-of-set? x (cdr set)))))
+
+;; (define (adjoin-set x set)
+;;   (if (element-of-set? x set)
+;;       set
+;;       (cons x set)))
+
+;; (define (intersection-set set1 set2)
+;;   (cond ((or (null? set1) (null? set2)) '())
+;;         ((element-of-set? (car set1) set2)
+;;          (cons (car set1)
+;;                (intersection-set (cdr set1) set2)))
+;;         (else (intersection-set (cdr set1) set2))))
+
+;;-----------------------------------------------------------------------------
+;; Exercise 2.59
+;; -------------
+
+;; (define (union-set set1 set2)
+;;   (cond ((null? set1) set2)
+;;         ((null? set2) set1)
+;;         (else (adjoin-set (car set1) (union-set (cdr set1) set2))) ))
+
+
+;;-----------------------------------------------------------------------------
+;; Exercise 2.60
+;; -------------
+
+;; (define (element-of-set? x set)
+;;   (cond ((null? set) false)
+;;         ((equal? x (car set)) true)
+;;         (else (element-of-set? x (cdr set)))))
+
+;; (define (adjoin-set x set)
+;;   (cons x set))
+
+;; (define (intersection-set set1 set2)
+;;   (cond ((or (null? set1) (null? set2)) '())
+;;         ((element-of-set? (car set1) set2)
+;;          (cons (car set1)
+;;                (intersection-set (cdr set1) set2)))
+;;         (else (intersection-set (cdr set1) set2))))
+
+;; (define (union-set set1 set2)
+;;   (append set1 set2) )
+
+;;-----------------------------------------------------------------------------
+
+;; (define (element-of-set? x set)
+;;   (cond ((null? set) false)
+;;         ((= x (car set)) true)
+;;         ((< x (car set)) false)
+;;         (else (element-of-set? x (cdr set)))))
+
+;; (define (intersection-set set1 set2)
+;;   (if (or (null? set1) (null? set2))
+;;       '()
+;;       (let ((x1 (car set1)) (x2 (car set2)))
+;;         (cond ((= x1 x2)
+;;                (cons x1
+;;                      (intersection-set (cdr set1)
+;;                                        (cdr set2))))
+;;               ((< x1 x2)
+;;                (intersection-set (cdr set1) set2))
+;;               ((< x2 x1)
+;;                (intersection-set set1 (cdr set2)))))))
+
+;;-----------------------------------------------------------------------------
+;; Exercise 2.61
+;; -------------
+
+;; (define (adjoin-set x set)
+;;   (cond ((null? set) (list x))
+;;         ((< x (car set)) (cons x set))
+;;         ((= x (car set)) set)
+;;         (else            (cons (car set) (adjoin-set x (cdr set)))) ))
+
+;;-----------------------------------------------------------------------------
+;; Exercise 2.62
+;; -------------
+
+;; (define (union-set set1 set2)
+;;   (cond ((null? set1)              set2)
+;;         ((null? set2)              set1)
+;;         ((< (car set1) (car set2)) (cons (car set1) (union-set (cdr set1) set2)))
+;;         ((= (car set1) (car set2)) (cons (car set1) (union-set (cdr set1) (cdr set2))))
+;;         (else                      (cons (car set2) (union-set set1 (cdr set2)))) ))
+
+;;-----------------------------------------------------------------------------
+
+(define (entry tree) (car tree))
+
+(define (left-branch tree) (cadr tree))
+
+(define (right-branch tree) (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (entry set)) true)
+        ((< x (entry set))
+         (element-of-set? x (left-branch set)))
+        ((> x (entry set))
+         (element-of-set? x (right-branch set)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree (entry set)
+                    (adjoin-set x (left-branch set))
+                    (right-branch set)))
+        ((> x (entry set))
+         (make-tree (entry set)
+                    (left-branch set)
+                    (adjoin-set x (right-branch set))))))
+
+
+;;-----------------------------------------------------------------------------
+;; Exercise 2.63
+;; -------------
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+              (cons (entry tree)
+                    (tree->list-1 (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-branch tree)
+                      (cons (entry tree)
+                            (copy-to-list (right-branch tree)
+                                          result-list)))))
+  (copy-to-list tree '()))
+
+(define set1 '(7 (3 (1 () ()) (5 () ())) (9 () (11 () ()))) )
+
+
+;;-----------------------------------------------------------------------------
+;; Exercise 2.64
+;; -------------
+
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result (partial-tree (cdr non-left-elts)
+                                              right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts (cdr right-result)))
+                (cons (make-tree this-entry left-tree right-tree)
+                      remaining-elts))))))))
+
+;;-----------------------------------------------------------------------------
+;; Exercise 2.65
+;; -------------
+
+(define (intersection-sorted-list set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1)) (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1
+                     (intersection-sorted-list (cdr set1)
+                                       (cdr set2))))
+              ((< x1 x2)
+               (intersection-sorted-list (cdr set1) set2))
+              ((< x2 x1)
+               (intersection-sorted-list set1 (cdr set2)))))))
+
+(define (intersection-set set1 set2)
+  (list->tree (intersection-sorted-list (tree->list-2 set1)
+                                        (tree->list-2 set2))))
+
+;;-----------------------------------------------------------------------------
+;; Exercise 2.65
+;; -------------
+
+(define (lookup given-key set-of-records)
+  (if (null? set-of-records)
+      #f
+      (let ((e (entry set-of-records))
+            (k (key (entry set-of-records))))
+        (cond ((= given-key k) e)
+              ((< given-key k) (lookup given-key (left-branch set-of-records)))
+              (else            (lookup given-key (right-branch set-of-records))) ))))
+
+(define key identity)
+
+
 
 
 
